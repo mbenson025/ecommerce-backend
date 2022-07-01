d33b1bfc7d5200213f00b9826ed36a1e63c4b4cb
mbenson025 <mbenson025@gmail.com>
2022-07-01 15:49:56 -0500

    tag routes get, get id, post

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,17 +4,40 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product],
+  })
+    .then((tagData) => res.json(tagData))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((tagData) => {
+      if (!tagData) {
+        res.status(404).json({ message: 'Tag not found' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+  })
+    .then((tagData) => res.json(tagData))
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
