48d61dcd31f1a9526017cbe9abb281be078e92d8
mbenson025 <mbenson025@gmail.com>
2022-06-30 22:59:34 -0500

    mittens category finally added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,20 +4,35 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  const cData = Category.findAll({
+  Category.findAll({
     include: [Product],
   })
-    .then((categories) => res.json(categories))
+    .then((cData) => res.json(cData))
     .catch((err) => res.status(500).json(err));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product],
+  })
+    .then((cData) => res.json(cData))
+    .catch((err) => res.status(500).json(err));
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((cData) => res.json(cData))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
