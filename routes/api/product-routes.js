4ff509237704a24659f18f9644adcaf79d7bf273
mbenson025 <mbenson025@gmail.com>
2022-07-01 13:52:09 -0500

    async await gets on product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,25 +4,50 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const pData = await Product.findAll({
+      include: [
+        { model: Category, attributes: ['category_name'] },
+        { model: Tag, attributes: ['tag_name'] },
+      ],
+    });
+    res.json(pData);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const pData = await Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        { model: Category, attributes: ['category_name'] },
+        { model: Tag, attributes: ['tag_name'] },
+      ],
+    });
+    res.json(pData);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./routes');
 
-// import sequelize connection
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
