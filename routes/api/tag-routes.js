497e25ae9d59b07ecc59e48ae00e807dbd144fdb
mbenson025 <mbenson025@gmail.com>
2022-07-01 19:39:05 -0500

    finished CRUD requests on tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -41,11 +41,44 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((tagData) => {
+      if (!tagData) {
+        res.status(404).json({ message: 'Tag id not found' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tagData) => {
+      if (!tagData) {
+        res.json(404).json({ message: 'Tag id not found' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
